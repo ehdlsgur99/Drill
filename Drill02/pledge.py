ffcd5b0021feb714a6df98cfee3897368337b18f
doislike <ehdlsgur99@naver.com>
2021-09-07 17:53:36 +0900

    Drill02
    2018180014 도인혁

--- Drill02/pledge.py
+++ Drill02/pledge.py
@@ -1,9 +1,6 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 2018180014 도인혁
-SyntaxError: invalid syntax
->>> print('2018180014 도인혁 ')
-2018180014 도인혁 
->>> print('각오 : 1학년때부터 궁금했던 강의입니다. 최선을 다해서 수강하겠습니다! 감사합니다!')
-각오 : 1학년때부터 궁금했던 강의입니다. 최선을 다해서 수강하겠습니다! 감사합니다!
->>> 
+# 학번: 2018180014
+# 이름: 도인혁
+# 2DGP 수업을 임하는 각오: 1학년때부터 궁금했던 수업입니다. 최선을 다해서 수강하겠습니다!
+
+print("Hello, World!")
+
